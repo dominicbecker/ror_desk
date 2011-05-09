a637c6c4451b4750384a17be2a49b87deafb5da9
Dominic Becker <dominic.r.becker@gmail.com>
2011-05-09 16:46:05 -0700

    Corrected a capitalization typo that prevented install from working correctly.

--- install.rb
+++ install.rb
@@ -2,5 +2,5 @@ require 'fileutils'
 
 src = File.join(File.dirname(__FILE__), 'config', 'odesk.yml')
 target = File.join(Rails.root, 'config', 'odesk.yml')
-Fileutils.cp src, target unless File.exists?(target)
+FileUtils.cp src, target unless File.exists?(target)
 
