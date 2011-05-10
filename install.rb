a87c1ded9e9436d990969bdf24eaa6c1771af02d
Dominic Becker <dominic.r.becker@gmail.com>
2011-05-09 17:56:55 -0700

    Commented attempt to append to Gemfile, updated readme for Rails 3 users. Temporary fix, ideally.

--- install.rb
+++ install.rb
@@ -1,14 +1,13 @@
-require 'fileutils'
+require 'ftools'
 
 src = File.join(File.dirname(__FILE__), 'config', 'odesk.yml')
 target = File.join(Rails.root, 'config', 'odesk.yml')
 FileUtils.cp src, target unless File.exists?(target)
 
 # Add line to Gemfile if there is a Gemfile to require ruby_desk
-gfname = File.join(Rails.root, 'Gemfile')
-puts gfname
-if File.exists(gfname)
-  gf = File.open(gfname, 'a')
-  gf.puts("\ngem \'ruby_desk\'")
-  gf.close
-end
+#gfname = File.join(Rails.root, 'Gemfile')
+#if File.exists(gfname)
+#  gf = File.open(gfname, 'a')
+#  gf.puts("\ngem \'ruby_desk\'")
+#  gf.close
+#end
